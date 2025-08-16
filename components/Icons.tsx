a41bed1bf12f4313dfbc7638b85a7203a9af4b68
Randy Harkediansa <randy.harkediansa@gmail.com>
2025-08-16 22:19:49 +0700

    add Icon component

---
+++ components/Icons.tsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+export const HomeIcon: React.FC = () => (
+  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+);
+
+export const DumbbellIcon: React.FC = () => (
+    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 4.35c0-1.65-1.35-3-3-3s-3 1.35-3 3v15c0 1.65 1.35 3 3 3s3-1.35 3-3V4.35zM3 4.35c0-1.65 1.35-3 3-3s3 1.35 3 3v15c0 1.65-1.35 3-3 3S3 20.65 3 19V4.35zM9 12h6"></path></svg>
+);
+
+export const ClipboardListIcon: React.FC = () => (
+  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+);
+
+export const ChartBarIcon: React.FC = () => (
+  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+);
+
+export const SparklesIcon: React.FC = () => (
+  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M19 3v4M17 5h4M14 11l-1.46-1.46a.5.5 0 00-.71 0L10 11l-1.46-1.46a.5.5 0 00-.71 0L6 11l-1.46-1.46a.5.5 0 00-.71 0L2 11m12 10l-1.46-1.46a.5.5 0 00-.71 0L10 21l-1.46-1.46a.5.5 0 00-.71 0L6 21l-1.46-1.46a.5.5 0 00-.71 0L2 21m20-10l-1.46-1.46a.5.5 0 00-.71 0L18 11l-1.46-1.46a.5.5 0 00-.71 0L14 11l-1.46-1.46a.5.5 0 00-.71 0L10 11"></path></svg>
+);
+
+export const LockClosedIcon: React.FC = () => (
+  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>
+);
